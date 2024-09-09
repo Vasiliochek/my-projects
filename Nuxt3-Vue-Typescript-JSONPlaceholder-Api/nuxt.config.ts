2a11369007da669bc698f612f2722a21be63659f
Vasily Makarov <makarov.vasia@gmail.com>
2024-09-09 15:36:38 +0400

    transfer

--- Nuxt3-Vue-Typescript-JSONPlaceholder-Api/nuxt.config.ts
+++
@@ -1,6 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxt/image'],
-  devtools: { enabled: false },
-  compatibilityDate: '2024-09-06'
-})
